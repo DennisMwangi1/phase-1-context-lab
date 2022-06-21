aaf246be47a992d2f00ac47ab8a8fe7c23ba7fcf
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-21 20:46:09 +0300

    Done.

--- index.js
+++ index.js
@@ -20,4 +20,67 @@ const allWagesFor = function () {
 
     return payable
 }
+const createEmployeeRecord = (array)=>{
+    return{
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+const createEmployeeRecords = (employeeData)=>{
+    return employeeData.map((array)=>{
+        return createEmployeeRecord(array)
+    })
+}
+let createTimeInEvent = function(dateLabel){
+    let [date, hour] = dateLabel.split(' ')
 
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let createTimeOutEvent = function(dateLabel){
+    let [date, hour] = dateLabel.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+
+let hoursWorkedOnDate = function(wantedDate){
+    let arriving = this.timeInEvents.find(function(e){
+        return e.date === wantedDate
+    })
+
+    let leaving = this.timeOutEvents.find(function(e){
+        return e.date === wantedDate
+    })
+
+    return (leaving.hour - arriving.hour) / 100
+}
+let wagesEarnedOnDate = function(wantedDate){
+    let Wage = hoursWorkedOnDate.call(this, wantedDate)
+        * this.payPerHour
+    return parseFloat(Wage.toString())
+}
+let findEmployeeByFirstName = function(array, firstName) {
+    return array.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+  let calculatePayroll = function(arrayOfEmployees){
+    return arrayOfEmployees.reduce(function(memo, record){
+        return memo + allWagesFor.call(record)
+    }, 0)
+}
